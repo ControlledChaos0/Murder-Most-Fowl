c34e452c96bf44e44f41fa2a29b1bff6c02578cf
Samad <143888385+Poke1155@users.noreply.github.com>
2025-03-06 19:12:52 -0500

    combinations
    clue combinations

---
+++ Assets/Scripts/Clues/ClueCombinationsDatabase.cs
@@ -0,0 +1,13 @@
+// using UnityEngine;
+// using Utils.SerializableDictionary;
+//
+// namespace Clues.ClueCombinations
+// {
+//     [CreateAssetMenu(fileName = "Clue Combination Database", menuName = "Scriptable Objects/Clue Combinations")]
+//     public class ClueCombinationsDatabase : ScriptableObject
+//     {
+//         [SerializeField] public SerializableDictionary<string, string> Combinations;
+//     }
+// }
+
+// may get used later, probably not
