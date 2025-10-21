e194f7354f847b9e9b3c4fa391640fd03d3ca1b1
Sophia Regnault <sophiaregnault@icloud.com>
2025-10-21 16:58:55 -0400

    More Rooms

--- Assets/Scripts/TrainCar/RoomTeleport.cs
+++ Assets/Scripts/TrainCar/RoomTeleport.cs
@@ -13,7 +13,7 @@ public class RoomTeleport : MonoBehaviour
     [SerializeField]
     private CinemachineVirtualCamera _currCamera;
     [SerializeField]
-    private Vector3 _roomTarget;
+    private Transform _door;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -26,7 +26,8 @@ public class RoomTeleport : MonoBehaviour
         if (_room != null && _player != null)
         {
             ScreenManager.Instance.ChangeRooms(_room);
-            _player.Teleport(_roomTarget);
+            
+            _player.Teleport(new Vector3(_door.transform.position.x, _door.transform.position.y - 5, _door.transform.position.z));
             _newCamera.Priority = 10;
             _currCamera.Priority = 1;
         }
