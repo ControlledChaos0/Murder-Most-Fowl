cb55567d9ecda7326c0b66ec692c641c09e4145e
Sophia Regnault <sophiaregnault@icloud.com>
2026-02-27 17:07:45 -0500

    Yarn Ending Adjustments

--- Assets/Scripts/UI/Credits Scroll.cs
+++ Assets/Scripts/UI/Credits Scroll.cs
@@ -1,9 +1,11 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class CreditsScroll : MonoBehaviour
 {
     public float scrollSpeed = 40.0f;
+    public float endWait = 3.0f;
     public float startY = -841;
     public float endY = 1961;
     private RectTransform scrollWindow;
@@ -20,6 +22,17 @@ public class CreditsScroll : MonoBehaviour
         if (scrollWindow.position.y < endY)
         {
             scrollWindow.anchoredPosition += new Vector2(0, scrollSpeed * Time.deltaTime);
+        } else
+        {
+            SceneManager sceneManager = FindFirstObjectByType<SceneManager>();
+            sceneManager.LoadSceneAndSwapTransition("Main Menu");
         }
     }
+
+    // public IEnumerator EndCredits()
+    // {
+    //     yield return new WaitForSeconds(endWait);
+    //     SceneManager sceneManager = FindFirstObjectByType<SceneManager>();
+    //     sceneManager.LoadSceneAndSwapTransition("Main Menu");
+    // }
 }
